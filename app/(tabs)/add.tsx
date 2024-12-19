0484bcdb6ffaee86ad8efd26cad8174d5ba7863b
lucidmach <lucidmach@gmail.com>
2024-12-19 17:49:48 +1100

    creating an expense - selecting contacts from phone 🤟

--- app/(tabs)/add.tsx
+++ app/(tabs)/add.tsx
@@ -73,6 +73,108 @@ export default function AddScreen() {
           padding: 12,
         }}
       />
+      {selectedContacts.length > 0 ? (
+        <FlatList
+          style={{ flexGrow: 0, width: "80%", margin: 12 }}
+          data={selectedContacts}
+          renderItem={({ item }) => (
+            <TouchableOpacity
+              onPress={() => {
+                selectContacts((contacts) =>
+                  contacts.filter((contact) => contact !== item)
+                );
+              }}
+            >
+              <Text
+                style={{
+                  color: "#f1f1f1",
+                  margin: 12,
+                  borderColor: "#5CE4C7",
+                  borderWidth: 2,
+                  textAlign: "center",
+                  padding: 8,
+                  borderRadius: 200,
+                }}
+              >
+                {item.name ? item.name.toString() : ""}
+              </Text>
+            </TouchableOpacity>
+          )}
+        />
+      ) : null}
+      <TouchableOpacity
+        style={{
+          width: "80%",
+          backgroundColor: "#5CE4C7",
+          justifyContent: "center",
+          alignItems: "center",
+          padding: 12,
+          margin: 12,
+          borderRadius: 200,
+        }}
+        onPress={() => setSearchContacts((search) => !search)}
+      >
+        <Text>select people</Text>
+      </TouchableOpacity>
+      {searchContacts ? (
+        <Modal
+          animationType="slide"
+          visible={searchContacts}
+          onRequestClose={() => {
+            Alert.alert("Modal has been closed.");
+            setSearchContacts(!searchContacts);
+          }}
+        >
+          <View
+            style={{
+              height: "100%",
+              backgroundColor: "#1a1a1a",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <Text style={{ color: "#f1f1f1", margin: 12 }}>people</Text>
+            <FlatList
+              data={phoneContacts}
+              renderItem={({ item }) => (
+                <TouchableOpacity
+                  onPress={() => {
+                    selectContacts((contacts) => [...contacts, item]);
+                    setSearchContacts(!searchContacts);
+                  }}
+                >
+                  <Text
+                    style={{
+                      color: "#f1f1f1",
+                      margin: 16,
+                      fontSize: 16,
+                    }}
+                  >
+                    {item.name ? item.name.toString() : ""}
+                  </Text>
+                </TouchableOpacity>
+              )}
+            />
+            <TouchableOpacity
+              onPress={() => setSearchContacts(!searchContacts)}
+            >
+              <Text
+                style={{
+                  margin: 16,
+                  fontSize: 20,
+                  paddingHorizontal: 32,
+                  paddingVertical: 4,
+                  borderRadius: 32,
+                  color: "#1a1a1a",
+                  backgroundColor: "#5CE4C7",
+                }}
+              >
+                close
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </Modal>
+      ) : null}
     </View>
   );
 }
